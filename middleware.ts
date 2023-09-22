e39aee84aa4425d161307441a8e01e7630cd84f1
F1x <f1x777111@gmail.com>
2023-09-22 03:14:58 +0300

    map fix

--- middleware.ts
+++ middleware.ts
@@ -7,25 +7,28 @@ const rateLimit = {
 
 const requestCounts = new Map<string, { count: number; resetTime: number }>();
 
-export default async function middleware(request: NextRequest) {
+export const middleware = async (request: NextRequest) => {
   const clientIp = request.ip;
   if (!clientIp) return;
 
   if (!requestCounts.has(clientIp))
     requestCounts.set(clientIp, { count: 0, resetTime: Date.now() + rateLimit.windowMs });
 
-  let clientData = requestCounts.get(clientIp);
+  const clientData = requestCounts.get(clientIp);
   const currentTime = Date.now();
 
   if (!clientData) return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
 
-  if (currentTime > clientData.resetTime) {
-    clientData = { ...clientData, count: 0, resetTime: currentTime + rateLimit.windowMs };
-    requestCounts.set(clientIp, clientData);
-  }
+  if (currentTime > clientData.resetTime)
+    requestCounts.set(clientIp, { ...clientData, count: 0, resetTime: currentTime + rateLimit.windowMs });
 
-  return NextResponse.json({ error: 'Rate limit exceeded. Please try again later.' }, { status: 429 });
-}
+  if (clientData.count >= rateLimit.maxRequests)
+    return NextResponse.json({ error: 'Rate limit exceeded. Please try again later.' }, { status: 429 });
+
+  requestCounts.set(clientIp, { ...clientData, count: clientData.count + 1 });
+
+  return NextResponse.next();
+};
 
 export const config = {
   matcher: '/api/message/:path*',
